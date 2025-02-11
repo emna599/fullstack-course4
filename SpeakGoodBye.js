4b161cf45bf54243ce8a29f960a9bb06fdbfb917
emna599 <emna.belkahla001@gmail.com>
2025-02-11 22:29:52 +0100

    module4-sol

---
+++ SpeakGoodBye.js
@@ -0,0 +1,7 @@
+(function (window) {
+    var byeSpeaker = {};
+    byeSpeaker.speak = function (name) {
+      console.log("Goodbye " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+  })(window);
